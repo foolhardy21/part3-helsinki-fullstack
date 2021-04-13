bdb9e3a7e7af9cb08502f87744049435903029d6
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-04-13 18:57:54 +0530

    phonebook-backend3.1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+      "name": "Arto Hellas",
+      "number": "040-44-1234567",
+      "id": 1
+    },
+    {
+      "name": "Rahul Kohli",
+      "number": "089-12342111",
+      "id": 2
+    },
+    {
+      "name": "Bear Jew",
+      "number": "022-49-1234567",
+      "id": 3
+    },
+    {
+      "name": "Emmanuel Skarsgard",
+      "number": "067-98-1234567",
+      "id": 4
+    }
+]
+
+app.get('/api/persons',(request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT,() => {
+  console.log(`Server running on port ${PORT}`)
+})
